bdcbf41e46ea888f6d317e825caf22926b2268b5
ixslea <83837721+ixslea@users.noreply.github.com>
2025-08-17 21:58:35 +0300

    Update settings.py

--- test_its_bai/test_its_bai/settings.py
+++ test_its_bai/test_its_bai/settings.py
@@ -45,15 +45,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles',
-    
 ]
 
 ROOT_URLCONF = 'test_its_bai.urls'
