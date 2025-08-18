838b5c771524418d3554999230fb78f1bbc0281f
ixslea <83837721+ixslea@users.noreply.github.com>
2025-08-19 00:54:14 +0300

    Update models.py

--- test_its_bai/live_love_laugh/models.py
+++ test_its_bai/live_love_laugh/models.py
@@ -30,7 +30,7 @@ class Quote(models.Model):
         """
         if not session_key:
             return False
-        return self.like_set.filter(session_key=session_key).exists()
+        return self.total_likes.filter(session_key=session_key).exists()
 
 
     @classmethod
